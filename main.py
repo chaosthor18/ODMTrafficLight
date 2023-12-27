f1a226220ee7be494daf5e8d7e9d09690e5a9087
unknown <casaljayreydentor@gmail.com>
2023-12-27 14:42:29 +0800

    change in variables

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cvzone
 import math
 import time
 
-cap = cv2.VideoCapture("./Videos/vehicle1.mp4")
+cap = cv2.VideoCapture("./Videos/mixvehicle.mp4")
 # cap.set(3, 640)
 # cap.set(4, 480)
 
@@ -13,35 +13,47 @@ model = YOLO('../yolo-weights/yolov5n6u.pt')
 
 #global variables
 gologicd2_seconds = 0 #duration of go of logic device 2
-fvehicledetected_sec = 0 #number of second first vehicle detected
+fvehicledetected_sec = 0 #number of seconds first vehicle detected
 frames_sufvehicle = 0 # number of frames that has sufficient vehicles
 
 #SAMPLE TRAFFIC SIGNAL
 logic_device1 = False
 logic_device2 = True
 
-#timings -1 every seconds
-goandstop_timing = 29
-yellow_timing =14
+#timings (adjustable) constant
+GOANDSTOP_TIMING = 30
+YELLOW_TIMING = 15
+#timings for exception condition
+EXCEPTION_TIMING = 40
+
+#number of vehicles
+LIM_BVEHICLE = 2 # trigger value big vehicles
+LIM_SVEHICLE = 3 # trigger value small vehicles
+LIM_TVEHICLE = 3 # trigger value total vehicles
+
+#adjust required frames to trigger true in check vehicle function
+FRAMES_REQUIRED = 5
+
 
 def check_vehicle(count_bvehicle, count_svehicle):
     global frames_sufvehicle
     global fvehicledetected_sec
     totalVehicles = count_bvehicle + count_svehicle
-    if(frames_sufvehicle>=4 or fvehicledetected_sec >= 40):
+    if(frames_sufvehicle>=FRAMES_REQUIRED or fvehicledetected_sec >= EXCEPTION_TIMING):
         frames_sufvehicle = frames_sufvehicle * 0
         fvehicledetected_sec = fvehicledetected_sec * 0
         return True
-    if (totalVehicles>= 4):
+    if (totalVehicles>= LIM_TVEHICLE):
+        frames_sufvehicle = frames_sufvehicle + 1
+        print("FRAMES HAVE SUFFC: %s" % frames_sufvehicle)
+        return False
+    elif(count_bvehicle >= LIM_BVEHICLE ): # big vehicle is greater than or equal to 3
         frames_sufvehicle = frames_sufvehicle + 1
         return False
-    elif(count_bvehicle >= 3 ): # big vehicle is greater than or equal to 3
+    elif(count_svehicle >= LIM_SVEHICLE): # small vehicle is greater than or equal to 4
         frames_sufvehicle = frames_sufvehicle + 1
         return False
-    elif(count_svehicle >= 4): # small vehicle is greater than or equal to 4
-        frames_sufvehicle = frames_sufvehicle + 1
-        return False
-    elif(totalVehicles!=0):
+    elif(totalVehicles!=0): #check if not empty only if 50 seconds pass
         fvehicledetected_sec = fvehicledetected_sec + 1
         return False
     else:
@@ -62,13 +74,15 @@ def yellow_signal():
 def go_logicdevice1():
     global logic_device1
     global logic_device2
+    print("Traffic Light 1: %s" % logic_device1)
+    print("Traffic Light 2: %s" % logic_device2)
     # green duration of logic device 1
     if (logic_device1 == True):
-        for sec in range(goandstop_timing+1):
+        for sec in range(GOANDSTOP_TIMING):
             print("Go: %s" % (sec+1))
             time.sleep(1)
         yellow_signal()
-        for sec in range(yellow_timing+1):
+        for sec in range(YELLOW_TIMING):
             print("Yellow: %s" % (sec+1))
             time.sleep(1)
         logic_device1 = not logic_device1
@@ -78,17 +92,16 @@ def signal_change(check_vehicle): # TRUE=GO FALSE=STOP
     global logic_device1
     global logic_device2
     global gologicd2_seconds
-    if(check_vehicle==True and gologicd2_seconds>=goandstop_timing): #CHANGE SIGNAL STATUS
+    if(check_vehicle==True and gologicd2_seconds>=GOANDSTOP_TIMING-1): #CHANGE SIGNAL STATUS
         yellow_signal()
-        for sec in range(yellow_timing+1): #15 seconds yellow
+        for sec in range(YELLOW_TIMING): #15 seconds yellow
             print("Yellow: %s" % (sec+1))
             time.sleep(1)
         #signal change
         logic_device1= not logic_device1
         logic_device2= not logic_device2
-        if(gologicd2_seconds>=goandstop_timing):
-            gologicd2_seconds = gologicd2_seconds * 0
-            go_logicdevice1()
+        gologicd2_seconds = gologicd2_seconds * 0
+        go_logicdevice1()
         print("Traffic Light 1: %s" % logic_device1)
         print("Traffic Light 2: %s" % logic_device2)
     else:
@@ -134,9 +147,11 @@ def main():
                         vehicle_class2 = vehicle_class2 + 1
                 # print(confidence)
                 # print(x1, y1, x2, y2)
+            check_v = check_vehicle(vehicle_class1, vehicle_class2)
             print("Big vehicle: %s  Small vehicle: %s" %(vehicle_class1,vehicle_class2))
-            print("Trigger Signal Change: %s" %(check_vehicle(vehicle_class1, vehicle_class2)))
-            signal_change(check_vehicle(vehicle_class1, vehicle_class2))
+            print("Trigger Signal Change: %s" %(check_v))
+            #signal_change(check_vehicle(vehicle_class1, vehicle_class2))
+            signal_change(check_v)
             print("Go duration(logic device 2): %s"%gologicd2_seconds)
          # cv2.imshow("Video", img)
          # cv2.waitKey(1)
